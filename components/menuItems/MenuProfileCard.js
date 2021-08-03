db0f570782ab4340011abb58b8b4fa3b3af21203
Justin <53011686+jsgsxr@users.noreply.github.com>
2021-08-03 15:27:40 +0800

    Trying to fix session data in menu

--- components/menuItems/MenuProfileCard.js
+++ components/menuItems/MenuProfileCard.js
@@ -1,15 +1,17 @@
 import styles from '../../styles/userMenu.module.css'
+import { useSession } from 'next-auth/client'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function MenuSignOut(props) {
+export default function MenuSignOut() {
+  const [session, loading] = useSession()
 
   return (
     <Link href="/comingSoon" passHref={true}>
       <div className={styles.menuItemDiv}>
-        <Image className={styles.menuProfileImg} src={props.session.user.image} width="60px" height="60px" alt="profile" />
+        <Image className={styles.menuProfileImg} src={session.user.image} width="60px" height="60px" alt="profile" />
         <div className={styles.itemDividerDiv}>
-          <p className={styles.menuProfileName}>{props.session.user.name}</p>
+          <p className={styles.menuProfileName}>{session.user.name}</p>
           <p className={styles.menuItemSubText}>Edit Your Profile</p>  
         </div>
       </div>
--- pages/userMenu.js
+++ pages/userMenu.js
@@ -1,18 +1,16 @@
 import Layout from '../components/layouts'
 import styles from '../styles/userMenu.module.css'
-import { useSession } from 'next-auth/client'
 import MenuItems from '../components/menuItems/MenuItems'
 import menuData from '../config/menuData'
 import MenuSignOut from '../components/menuItems/MenuSignOut'
 import MenuProfileCard from '../components/menuItems/MenuProfileCard'
 
-export default function UserMenu() {
-  const [session, loading] = useSession()
+export default function UserMenu(props) {
 
   return (
     <Layout>
       <div className={styles.menuMainDiv}>
-        <MenuProfileCard session={session} />
+        <MenuProfileCard />
         <MenuItems menuData={menuData} />
         <MenuSignOut />
       </div>
